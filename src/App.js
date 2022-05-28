cb3deca2c134b75128d15be7b8dc6efdfc9fd1fe
Simon Schuetze <simonschuetze@me.com>
2022-05-28 11:51:31 +0200

    Configure linting and theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import styled from 'styled-components';
+import React from 'react';
 
 const Box = styled.div`
   background: ${({ theme }) => theme.palette.background.light};
-  width: 100%;
   padding: 2rem;
   color: ${({ theme }) => theme.palette.text.main};
 `;
 
 function App() {
-  return (
-    <Box>
-      IQ-TranS
-    </Box>
-  );
+  return <Box>IQ-TranS</Box>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { ThemeProvider } from 'styled-components';
 import theme, { GlobalStyle, gridTheme } from 'styles/theme';
 import { GridThemeProvider } from 'styled-bootstrap-grid';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +17,7 @@ root.render(
         <App />
       </GridThemeProvider>
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
